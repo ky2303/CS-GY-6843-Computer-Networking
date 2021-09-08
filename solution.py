8aed0fabe3dfd05f49c124969f58ccf13161fde5
ky2303 <ky2303@nyu.edu>
2021-09-07 20:11:34 -0400

    2

--- solution.py
+++ solution.py
@@ -18,11 +18,11 @@ def welcome_assignment_answers(question):
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking":
         answer = "42b76fe51778764973077a5a94056724"
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 1
+        answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 2
+        answer = 4
     return(answer)
 # Complete all the questions.
 
